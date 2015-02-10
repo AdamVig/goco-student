674be586940bc84e4d771ab6adb088a706c88c50
Adam Vigneaux <adamvigneaux@gmail.com>
2015-02-10 03:05:59 -0500

    Add refreshData(), add menu modal

--- www/js/ChapelCreditController.js
+++ www/js/ChapelCreditController.js
@@ -1,30 +1,49 @@
-app.controller('ChapelCreditController', ['$state', '$filter', 'DataService', 'StorageService', function ($state, $filter, DataService, StorageService) {
+app.controller('ChapelCreditController', ['$scope', '$state', '$filter', '$timeout', '$ionicModal', 'DataService', 'StorageService', function ($scope, $state, $filter, $timeout, $ionicModal, DataService, StorageService) {
 
   var chapel = this;
+  if (window.AdMob) AdMob.showInterstitial();
   chapel.isLoading = true;
-  chapel.chapelCredit = {
-    credit: ""
-  };
+  chapel.chapelCredit = { credit: "" };
   chapel.userCredentials = StorageService.retrieveCredentials();
+  $ionicModal.fromTemplateUrl('html/_menu.html', {
+    scope: $scope
+  }).then(function(modal) {
+    chapel.modal = modal;
+  });
 
-  if (chapel.userCredentials != false) {
-    chapel.userName = $filter('NameFilter')(chapel.userCredentials.username);
+  chapel.refreshData = function () {
 
-    DataService.getChapelCredit(chapel.userCredentials).
+    if (chapel.modal) chapel.modal.hide();
+    chapel.isLoading = true;
+
+    if (chapel.userCredentials != false) {
+      chapel.userName = $filter('NameFilter')(chapel.userCredentials.username);
+
+      DataService.getChapelCredit(chapel.userCredentials).
       success(function(data) {
         chapel.chapelCredit.credit = data.credit;
         chapel.isLoading = false;
       }).
       error(function(data, status) {
         chapel.message = "Oops! Couldn't find your chapel credit.";
-        console.error("GET failed with error status", status);
+      }).
+      finally(function() {
         chapel.isLoading = false;
-      });
-  } else {
-    $state.go('login');
-  }
+     });
+
+    } else {
+      $state.go('login');
+    }
+  };
+
+  chapel.refreshData();
+
+  chapel.toggleModal = function() {
+    chapel.modal.isShown() ? chapel.modal.hide() : chapel.modal.show();
+  };
 
   chapel.logout = function () {
+    chapel.modal.hide();
     StorageService.eraseCredentials();
     $state.go('login');
   };
