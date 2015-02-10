0646476ae9dab84ea293dac1ff60e0142096b724
Adam Vigneaux <adamvigneaux@gmail.com>
2015-02-10 03:07:29 -0500

    Add ngMessages, remove StatusBar style setter

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,9 @@
-var app = angular.module('gocostudent', ['ionic']);
+var app = angular.module('gocostudent', ['ionic', 'ngMessages']);
 
 app.run(['$ionicPlatform', function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
     }
     if (window.AdMob) {
       AdMob.prepareInterstitial({
