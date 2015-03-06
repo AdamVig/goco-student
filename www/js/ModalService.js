21c0c48cc245cb2004a6a174a5666fd12210df70
Adam Vigneaux <adamvigneaux@gmail.com>
2015-03-06 01:55:16 -0500

    Add ModalService to create and maintain modals

---
+++ www/js/ModalService.js
@@ -0,0 +1,32 @@
+app.service('ModalService', ['$ionicModal', function ($ionicModal) {
+  var modalService = this;
+
+  this.createModals = function ($scope) {
+    // Menu modal
+    $ionicModal.fromTemplateUrl('html/_menu.html', {
+      scope: $scope
+    }).then(function(modal) {
+      modalService.menu = modal;
+    });
+
+    // Banner modal
+    $ionicModal.fromTemplateUrl('html/_banner.html', {
+      scope: null
+    }).then(function(modal) {
+      modalService.banner = modal;
+    });
+  };
+
+  this.toggleModal = function (modalName) {
+    modalService[modalName].isShown() ?
+      modalService[modalName].hide() :
+      modalService[modalName].show();
+  };
+
+  this.hideModal = function (modalName) {
+    if (modalService[modalName]) {
+      modalService[modalName].hide();
+    }
+  };
+
+}]);
