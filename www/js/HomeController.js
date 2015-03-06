0acd57e25e402898a1571b93dec297494d87b7e1
Adam Vigneaux <adamvigneaux@gmail.com>
2015-03-06 15:13:59 -0500

    Add getChapelCredits and getMealPoints

--- www/js/HomeController.js
+++ www/js/HomeController.js
@@ -1,6 +1,7 @@
-app.controller('HomeController', ['$scope', '$state', '$ionicModal', 'DatabaseFactory', 'ModalService', 'LogoutService', 'StorageService', function ($scope, $state, $ionicModal, DatabaseFactory, ModalService, LogoutService, StorageService) {
+app.controller('HomeController', ['$scope', '$state', '$ionicModal', 'DatabaseFactory', 'DataService', 'ModalService', 'LogoutService', 'StorageService', 'UsageService', function ($scope, $state, $ionicModal, DatabaseFactory, DataService, ModalService, LogoutService, StorageService, UsageService) {
 
   var home = this;
+  home.loading = {};
   $scope.logout = LogoutService;
   home.userCredentials = StorageService.retrieveCredentials();
   if (!home.userCredentials) $state.go('login');
@@ -17,5 +18,60 @@ app.controller('HomeController', ['$scope', '$state', '$ionicModal', 'DatabaseFa
       StorageService.storeBanner(response.data);
     }
   });
+
+  /**
+   * Handle $http error
+   * @param {object} data        Contains payload from $http response
+   * @param {number} status      HTTP status from $http response
+   * @param {string} description Either "chapel credit" or "meal points"
+   */
+  var handleError = function (data, status, description) {
+    if (status == 401) {
+      return "Username and password don't match. Log out and try again!"
+    } else if (status == 0) {
+      return "Oops! Couldn't find your " + description + ".";
+    } else {
+      console.error("Error getting data. Status:", status, "Response:", response);
+      return "Something went horribly wrong. Try again later!"
+    }
+  };
+
+  home.getChapelCredits = function () {
+    ModalService.hideModal('menu');
+    home.loading.chapelCredits = true;
+
+    // Get chapel credits
+    DataService.getChapelCredits(home.userCredentials).
+    success(function(data) {
+      home.chapelCredits = data.credit;
+      home.loading.chapelCredits = false;
+      UsageService.log(home.userCredentials.username);
+    }).
+    error(function(data, status) {
+      home.errorMessage.chapelCredits = handleError(data, status, "meal points");
+    }).
+    finally(function() {
+      home.loading.chapelCredits = false;
+    });
+  };
+
+  home.getMealPoints = function () {
+    ModalService.hideModal('menu');
+    home.loading.mealPoints = true;
+
+    // Get meal points
+    DataService.getMealPoints(home.userCredentials).
+    success(function(data) {
+      home.mealPoints = data.mealpoints;
+      home.loading.mealPoints = false;
+      UsageService.log(home.userCredentials.username);
+    }).
+    error(function(data, status) {
+      home.errorMessage.chapelCredits = handleError(data, status, "meal points");
+    }).
+    finally(function() {
+      home.loading.mealPoints = false;
+    });
+  };
 
 }]);
