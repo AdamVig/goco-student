089d2745d2d307c53085aae93318a120c37fe6c1
Adam Vigneaux <adamvigneaux@gmail.com>
2015-03-06 02:13:10 -0500

    Change MainController back to HomeController

--- www/js/HomeController.js
+++ www/js/HomeController.js
@@ -1,3 +1,12 @@
-app.controller('HomeController', [function () {
+app.controller('MainController', ['$scope', '$state', '$ionicModal', 'DatabaseFactory', 'ModalService', 'LogoutService', 'StorageService', function ($scope, $state, $ionicModal, DatabaseFactory, ModalService, LogoutService, StorageService) {
+
+  var home = this;
+  $scope.logout = LogoutService;
+
+  // Modals
+  ModalService.createModals($scope);
+  $scope.modal = ModalService;
+  $scope.refreshable = false;
+  $scope.banner = StorageService.retrieveBanner();
 
 }]);
