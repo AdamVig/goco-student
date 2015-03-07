0466a973a049bd2fa58b9add43667d20d82ac1c2
Adam Vigneaux <adamvigneaux@gmail.com>
2015-03-06 22:10:38 -0500

    Add showPopover(), fix things still named modal

--- www/js/PopoverService.js
+++ www/js/PopoverService.js
@@ -5,7 +5,7 @@ app.service('PopoverService', ['$ionicPopover', function ($ionicPopover) {
    * Create menu popover
    * @param {object} $scope Scope of controller
    */
-  this.createModals = function ($scope) {
+  this.createPopovers = function ($scope) {
     $ionicPopover.fromTemplateUrl('html/_menu.html', {
       scope: $scope,
     }).then(function(popover) {
@@ -14,10 +14,21 @@ app.service('PopoverService', ['$ionicPopover', function ($ionicPopover) {
   };
 
   /**
+   * Show a popover
+   * @param {String} popoverName Name of popover
+   * @param {Object} $event      Contains information from ng-click
+   */
+  this.showPopover = function (popoverName, $event) {
+    if (popoverService[popoverName]) {
+      popoverService[popoverName].show($event);
+    }
+  };
+
+  /**
    * Hide a popover
    * @param {String} popoverName Name of popover
    */
-  this.hideModal = function (popoverName) {
+  this.hidePopover = function (popoverName) {
     if (popoverService[popoverName]) {
       popoverService[popoverName].hide();
     }
