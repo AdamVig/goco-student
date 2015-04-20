c70b33573374c1be09f82ea1fab0c0f762eb3e35
Adam Vigneaux <adamvigneaux@gmail.com>
2015-04-19 22:37:30 -0400

    Add colors to App Update script output

--- appupdate.sh
+++ appupdate.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
+# Colors
+cecho() {
+  local code="\033["
+  case "$1" in
+    black  | bk) color="${code}0;30m";;
+    red    |  r) color="${code}1;31m";;
+    green  |  g) color="${code}1;32m";;
+    yellow |  y) color="${code}1;33m";;
+    blue   |  b) color="${code}1;34m";;
+    purple |  p) color="${code}1;35m";;
+    cyan   |  c) color="${code}1;36m";;
+    gray   | gr) color="${code}0;37m";;
+    *) local text="$1"
+  esac
+  [ -z "$text" ] && local text="$color$2${code}0m"
+  echo "$text"
+}
+
 devices=( "iPhone-4s" "iPhone-5" "iPhone-6" "iPhone-6-Plus" "iPad-Air" )
 
-echo "Welcome to AdamVig App Update."
+cecho cyan "Welcome to AdamVig App Update."
 
 # Emulate iOS devices
 read -p "Emulate all possible iOS devices? (y/n) " yn
@@ -11,7 +29,7 @@ if [ $yn == "y" ]; then
 
     # Emulate device
     cordova emulate ios --target="$i" >& /dev/null
-    echo "$i emulator started."
+    cecho blue "$i emulator started."
 
     # Bring iOS simulator window to front
     osascript \
@@ -30,7 +48,7 @@ read -p "Package Android app for distribution? (y/n) " yn
 if [ $yn == "y" ]; then
 
   # Build app for Android
-  echo "Building app for Android."
+  cecho blue "Building app for Android."
   cordova build --release android >& /dev/null && wait
 
   # Get keystore information
@@ -38,18 +56,18 @@ if [ $yn == "y" ]; then
   read -p "Keystore alias: " keystoreAlias
 
   # Sign APK
-  echo "Signing APK."
   jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
     -keystore $keystorePath \
+  cecho blue "Signing APK."
     platforms/android/ant-build/CordovaApp-release-unsigned.apk \
     $keystoreAlias
 
-  echo "Zipaligning and renaming APK."
   read -p "Name of app: " appName
+  cecho blue "Zipaligning and renaming APK."
   zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk \
     "$appName.apk"
 
-  echo "Done. Android app is ready for distribution."
+  cecho yellow  "Done. Android app is ready for distribution."
 fi
 
 # Package iOS app
@@ -57,12 +75,12 @@ read -p "Package iOS app for distribution? (y/n) " yn
 if [ $yn == "y" ]; then
 
   # Build app for iOS
-  echo "Building app for iOS."
+  cecho blue "Building app for iOS."
   cordova build --release ios >& /dev/null && wait
 
   # Open Xcode project
-  echo "Opening Xcode project."
+  cecho blue "Opening Xcode project."
   open platforms/ios/*.xcodeproj
 
-  echo "Done. iOS app is ready for distribution."
+  cecho yellow "Done. iOS app is ready for distribution."
 fi
