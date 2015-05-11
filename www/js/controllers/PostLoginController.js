53675617714a415cf57d674cbc06c6f86573445a
Adam Vigneaux <adamvigneaux@gmail.com>
2015-05-11 00:24:24 -0400

    Replace postLogin.go() with new, simpler routing functions

--- www/js/controllers/PostLoginController.js
+++ www/js/controllers/PostLoginController.js
@@ -1,9 +1,10 @@
-app.controller("PostLoginController", ["$timeout", "$state", "DataService", "StorageService", function ($timeout, $state, DataService, StorageService) {
+app.controller('PostLoginController', ['$timeout', '$state', 'DataService', 'StorageService', 'PopupService', function ($timeout, $state, DataService, StorageService, PopupService) {
 
   var postLogin = this,
     minimumCheckingTimeMs = 1000,
     maximumCheckingTimeMs = 5000;
   postLogin.status = "checking";
+  postLogin.user = {};
   postLogin.timeout = false;
   userCredentials = StorageService.retrieveCredentials();
 
@@ -33,9 +34,45 @@ app.controller("PostLoginController", ["$timeout", "$state", "DataService", "Sto
     postLogin.timeout = true;
   }, maximumCheckingTimeMs);
 
-  /* Go to a state */
-  postLogin.go = function (state) {
-    $state.go(state);
+  // Continue from privacy policy to home
+  postLogin.goFromPrivacyPolicy = function (accepted) {
+
+    // Accept privacy policy
+    if (accepted) {
+      DataService.setProperty(userCredentials, 'privacyPolicy', 'accepted')
+      .then(function () {
+        $state.go('home');
+      });
+
+    // Deny privacy policy
+    } else {
+      PopupService.showPopup('confirmOptOut')
+      .then(function (result) {
+        if (result) {
+          DataService.setProperty(userCredentials, 'privacyPolicy', 'denied')
+          .then(function () {
+            $state.go('home');
+          });
+        }
+      });
+    }
   };
 
+  // Continue from valid status to either privacy policy or home
+  postLogin.goHome = function () {
+
+    // Go home if privacy policy has been set
+    if (postLogin.user.privacyPolicy) {
+      $state.go('home');
+
+    // Show privacy policy if has not been set
+    } else {
+      postLogin.status = 'privacy-policy';
+    }
+  };
+
+  // Go to login screen
+  postLogin.goLogin = function () {
+    $state.go('login');
+  };
 }]);
