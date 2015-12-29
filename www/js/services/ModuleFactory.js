31bcbcc8e0fd0b3d09454c92abeeaca8c1be3042
Adam Vigneaux <adamvigneaux@gmail.com>
2015-12-29 18:09:02 -0500

    Add support for switching between module types
    - Store modules in object with child objects for each module type
    - Return only info pertinent to shown objects from getter functions
    - Add functions to change shown module type and get show module type

--- www/js/services/ModuleFactory.js
+++ www/js/services/ModuleFactory.js
@@ -7,16 +7,23 @@ app.factory('ModuleFactory', ['$rootScope', '$timeout', 'Modules', 'AppVersion',
   var _defaultModules = Modules;
   var _storedAppVersion = StorageService.get('version');
   var _currentAppVersion = AppVersion;
-  var _selectedModules = [];
-  var _numModules = 0;
-  var _moduleClass = '';
-  var _isScrollEnabled = false;
 
-  _allModules = _getAllModules();
-  _selectedModules = _getSelected(_allModules);
-  _numModules = _selectedModules.length;
-  _moduleClass = _getModuleClass(_numModules);
-  _isScrollEnabled = _getScrollEnabled(_numModules);
+  var _modules = {
+    'info': {
+      'selected': [],
+      'numSelected': 0,
+      'class': '',
+      'scrollEnabled': false,
+      'shown': true
+    },
+    'interaction': {
+      'selected': [],
+      'numSelected': 0,
+      'class': '',
+      'scrollEnabled': false,
+      'shown': false
+    }
+  };
 
   /**
    * Get all modules from either storage or constant
@@ -38,13 +45,24 @@ app.factory('ModuleFactory', ['$rootScope', '$timeout', 'Modules', 'AppVersion',
   }
 
   /**
+   * Get all shown modules
+   * @return {array}                    List of shown modules
+   */
+  function _getAllShownModules() {
+    return _allModules.filter(function (module) {
+      return _isModuleTypeShown(module.moduleType);
+    });
+  }
+
+  /**
    * Get selected modules from list of modules
    * @param  {array} modules Contains modules
+   * @param  {string} type   Type of modules to look for
    * @return {array}         Contains modules with 'selected': true
    */
-  function _getSelected(modules) {
+  function _getSelected(modules, type) {
     return modules.filter(function (module) {
-        return module.selected === true;
+        return module.selected === true && module.moduleType == type;
       });
   }
 
@@ -73,18 +91,62 @@ app.factory('ModuleFactory', ['$rootScope', '$timeout', 'Modules', 'AppVersion',
   }
 
   /**
+   * Change which module type is shown
+   * @param  {string} moduleType Type of module to show
+   */
+  function _changeModuleTypeShown(moduleType) {
+      _modules[moduleType].shown = true;
+
+      // Identify opposite module type and set 'shown' property to false
+      if (moduleType == 'info') {
+        _modules.interaction.shown = false;
+      } else {
+        _modules.info.shown = false;
+      }
+      moduleFactory.updateModules();
+  }
+
+  /**
+   * Return whether module type is shown or not
+   * @param  {string}  moduleType Type of module
+   * @return {Boolean}            Whether module type is shown or not
+   */
+  function _isModuleTypeShown(moduleType) {
+    return _modules[moduleType].shown;
+  }
+
+  /**
+   * Return shown module type
+   * @return {string}            Type of module that is currently shown
+   */
+  function _getModuleTypeShown() {
+    if (_modules.info.shown) {
+      return 'info';
+    } else {
+      return 'interaction';
+    }
+  }
+
+  /**
    * Update list of selected modules from list of all modules
    */
   moduleFactory.updateModules = function (modules) {
     _allModules = modules || _getAllModules();
-    _selectedModules = _getSelected(_allModules);
 
-    _numModules = _selectedModules.length;
-    _moduleClass = _getModuleClass(_numModules);
-    _isScrollEnabled = _getScrollEnabled(_numModules);
+    _modules.info.selected = _getSelected(_allModules, 'info');
+    _modules.interaction.selected = _getSelected(_allModules, 'interaction');
+
+    _modules.info.numSelected = _modules.info.selected.length;
+    _modules.interaction.numSelected = _modules.interaction.selected.length;
+
+    _modules.info.class = _getModuleClass(_modules.info.numSelected);
+    _modules.interaction.class = _getModuleClass(_modules.interaction.numSelected);
+
+    _modules.info.scrollEnabled = _getScrollEnabled(_modules.info.numSelected);
+    _modules.interaction.scrollEnabled = _getScrollEnabled(_modules.interaction.numSelected);
 
     StorageService.storeModules(_allModules);
-    $rootScope.$broadcast('modules:updated', _selectedModules);
+    $rootScope.$broadcast('modules:updated', _modules);
   };
 
   /**
@@ -96,11 +158,19 @@ app.factory('ModuleFactory', ['$rootScope', '$timeout', 'Modules', 'AppVersion',
   };
 
   /**
+   * Get all shown modules
+   * @return {array} List of module objects
+   */
+  moduleFactory.getAllShownModules = function () {
+    return _getAllShownModules();
+  };
+
+  /**
    * Get selected modules
    * @return {array}           List of selected modules
    */
   moduleFactory.getSelectedModules = function () {
-    return _selectedModules;
+    return _modules[_getModuleTypeShown()].selected;
   };
 
   /**
@@ -108,7 +178,7 @@ app.factory('ModuleFactory', ['$rootScope', '$timeout', 'Modules', 'AppVersion',
    * @return {String}           Class for modules determining their size
    */
   moduleFactory.getModuleClass = function () {
-    return _moduleClass;
+    return _modules[_getModuleTypeShown()].class;
   };
 
   /**
@@ -116,8 +186,27 @@ app.factory('ModuleFactory', ['$rootScope', '$timeout', 'Modules', 'AppVersion',
    * @return {Boolean} Whether scroll is enabled or not
    */
   moduleFactory.getScrollEnabled = function () {
-    return _isScrollEnabled;
+    return _modules[_getModuleTypeShown()].scrollEnabled;
   };
+
+  /**
+   * Change module type shown
+   * @param  {string} moduleType Type of module
+   */
+  moduleFactory.changeModuleTypeShown = function (moduleType) {
+    _changeModuleTypeShown(moduleType);
+  };
+
+  /**
+   * Return whether module type is shown or not
+   * @param  {string}  moduleType Type of module
+   * @return {Boolean}            Whether module type is shown or not
+   */
+  moduleFactory.isModuleTypeShown = function (moduleType) {
+    return _isModuleTypeShown(moduleType);
+  };
+
+  moduleFactory.updateModules();
 
   return moduleFactory;
 }]);
