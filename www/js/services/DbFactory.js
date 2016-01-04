fbd408cbb7eb442bb1528fb88ac5d5e4fa221223
Adam Vigneaux <adamvigneaux@gmail.com>
2016-01-03 21:36:49 -0500

    Add database initialization and general data retrieval

--- www/js/services/DbFactory.js
+++ www/js/services/DbFactory.js
@@ -1,9 +1,28 @@
-app.factory('DbService', [function () {
-  var dbService = this;
+app.factory('DbFactory', ['$ionicPlatform', '$q', function ($ionicPlatform, $q) {
 
-  var _db;
-  var _data;
+  var dbFactory = this;
+  var db;
+
+  // Create database object when device is ready
+  $ionicPlatform.ready(function() {
+    db = Lawnchair({name:'goco', record:'data'});
+  });
+
+  /**
+   * Get data from database
+   * @param  {string} key ID of data to get
+   * @return {promise}     Promise object fulfilled by retrieved data
+   */
+  var getData = function (key) {
+    var deferred = $q.defer();
+
+    db.get(key, function (data) {
+      deferred.resolve(data);
+    });
+
+    return deferred.promise;
+  };
 
 
-  return dbService;
+  return dbFactory;
 }]);
