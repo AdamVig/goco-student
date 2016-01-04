3c5194f5ddcc4820a18bb74f6fca35b68cbe26de
Adam Vigneaux <adamvigneaux@gmail.com>
2016-01-03 20:50:46 -0500

    Remove ngCordova in favor of Lawnchair

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('gocostudent', ['ionic', 'ngMessages', 'sc.twemoji', 'ngIOS9UIWebViewPatch', 'ngCordova']);
+var app = angular.module('gocostudent', ['ionic', 'ngMessages', 'sc.twemoji', 'ngIOS9UIWebViewPatch']);
 
 app.run(['$ionicPlatform', 'StorageService', 'AppVersion', function($ionicPlatform, StorageService, AppVersion) {
   $ionicPlatform.ready(function() {
