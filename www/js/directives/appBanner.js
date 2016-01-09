0e30d10e57e9a4bdca3547d683b7941e14b1804b
Adam Vigneaux <adamvigneaux@gmail.com>
2016-01-09 17:47:25 -0500

    Remove unneccesary logic from appBanner
    The directive was doing extra checks to make sure that the app
    info actually needed to be refreshed before it actually made the
    request. This is a holdover from when the app info was refreshed
    whenever the user tapped a module to load it. If the user tapped a bunch
    in a row, there would be many unnecessary requests for app info in a row.
    However, the logic now simply refreshes app info on load and on an
    interval defined by AppInfoRefreshTime after that.

--- www/js/directives/appBanner.js
+++ www/js/directives/appBanner.js
@@ -2,11 +2,9 @@ app.directive('appBanner', function () {
   return {
     templateUrl: 'html/directives/_appbanner.html',
     controllerAs: 'appBanner',
-    controller: ['$scope', '$ionicModal', '$interval', '$window', 'DataService', 'AppInfoRefreshTime', function ($scope, $ionicModal, $interval, $window, DataService, AppInfoRefreshTime) {
+    controller: ['$scope', '$ionicModal', '$interval', 'DataService', 'AppInfoRefreshTime', function ($scope, $ionicModal, $interval, DataService, AppInfoRefreshTime) {
 
       var appBanner = this;
-      var refreshCheckTime = 10000; // Time to check for refresh in ms
-      var lastRefreshTime = new Date(0);
 
       $ionicModal.fromTemplateUrl('html/_banner.html', {
         scope: $scope
@@ -24,24 +22,15 @@ app.directive('appBanner', function () {
 
       // Refresh app info including banner
       appBanner.refreshAppInfo = function () {
-        var now = new Date();
-        lastRefreshTime = new Date(
-            $window.localStorage.getItem('GoCo.lastRefresh'));
-
-        if (now - lastRefreshTime > AppInfoRefreshTime) {
-          lastRefreshTime = now;
-          $window.localStorage.setItem('GoCo.lastRefresh', lastRefreshTime);
-          DataService.get('AppInfo').then(function (response) {
-            if (response.data.banner.title) {
-              $scope.banner = response.data.banner;
-            }
-          });
-        }
+        DataService.get('AppInfo').then(function (response) {
+          if (response.data.banner.title) {
+            $scope.banner = response.data.banner;
+          }
+        });
       };
 
       appBanner.refreshAppInfo();
-
-      $interval(appBanner.refreshAppInfo, refreshCheckTime);
+      $interval(appBanner.refreshAppInfo, AppInfoRefreshTime);
     }]
   };
 });
