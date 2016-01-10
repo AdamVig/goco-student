e059ee40e578a733226f6fd2ad4e0bf656ad0d57
Adam Vigneaux <adamvigneaux@gmail.com>
2016-01-09 22:15:56 -0500

    Release version 2.5

--- www/js/constants.js
+++ www/js/constants.js
@@ -10,7 +10,7 @@ constant('ErrorMessages', {
 }).
 constant('DefaultSettings', {
   "loadOnLaunch": true,
-  "appVersion": '2.4'
+  "appVersion": '2.5'
 }).
 constant('DefaultModuleSettings', {
   'info': {
