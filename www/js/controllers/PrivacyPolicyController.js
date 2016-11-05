5f79155c9dfff3d437bc9711ec75e9d218178486
Adam Vigneaux <adamvigneaux@gmail.com>
2016-11-04 22:40:34 -0400

    Fix user privacy policy requests
    Setting the privacy policy was failing with a `409 Conflict` error code
    because the user data sent back to the API was stale. This problem could
    be fixed with better passing of data from `LoginController` to
    `PrivacyPolicyController`, but is good enough as is because the user
    will only have to accept the privacy policy once.

--- www/js/controllers/PrivacyPolicyController.js
+++ www/js/controllers/PrivacyPolicyController.js
@@ -5,7 +5,7 @@ app.controller("PrivacyPolicyController", ["$state", "$stateParams", "DataServic
   privacyPolicy.user = {};
   privacyPolicy.timeout = false;
   privacyPolicy.userCredentials = {};
-  privacyPolicy.user = $stateParams.user;
+  privacyPolicy.user = $stateParams.user; // Does not seem to work
 
   DbFactory.getCredentials().then(function (credentials) {
     privacyPolicy.userCredentials = credentials;
@@ -16,20 +16,38 @@ app.controller("PrivacyPolicyController", ["$state", "$stateParams", "DataServic
 
     // Accept privacy policy
     if (accepted) {
-      privacyPolicy.user.privacyPolicy = "accepted";
-      DataService.put("user/" + privacyPolicy.userCredentials.username,
-                      {data: privacyPolicy.user});
-      $state.go("home");
+      DataService
+        .get("user/" + privacyPolicy.userCredentials.username,
+             privacyPolicy.userCredentials.username)
+        .then(function (response) {
+          privacyPolicy.user = response.data.data;
+          privacyPolicy.user.privacyPolicy = "accepted";
+          return DataService.put("user/" +
+                                 privacyPolicy.userCredentials.username,
+                                 {data: privacyPolicy.user});
+        })
+        .then(function () {
+          $state.go("home");
+        });
 
     // Deny privacy policy
     } else {
       PopupService.showPopup("confirmOptOut")
       .then(function (result) {
         if (result) {
-          privacyPolicy.user.privacyPolicy = "denied";
-          DataService.put("user/" + privacyPolicy.userCredentials.username,
-                          {data: privacyPolicy.user});
-          $state.go("home");
+          DataService
+            .get("user/" + privacyPolicy.userCredentials.username,
+                 privacyPolicy.userCredentials.username)
+            .then(function (response) {
+              privacyPolicy.user = response.data.data;
+              privacyPolicy.user.privacyPolicy = "denied";
+              return DataService.put("user/" +
+                                     privacyPolicy.userCredentials.username,
+                                     {data: privacyPolicy.user});
+            })
+            .then(function () {
+              $state.go("home");
+            });
         }
       });
     }
